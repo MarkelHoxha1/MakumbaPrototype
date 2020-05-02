011e0db35e018f9d0f353df6f502f0dc108a38b8
Markel Hoxha <markel.hoxha4@outlook.com>
2020-05-02 18:02:56 +0200

    Deleted comment.

--- main.js
+++ main.js
@@ -44,27 +44,7 @@ class Query{
         return  new Query(this.q +string);
     }
     runQueries(){
-        // let arrayObjects = [];
-        // arrayObjects.push({ 
-        //     projections: ["line.name"],
-        //     querySections: ["ProductionLine line", undefined, null, null, null, null, null],
-        //     parentIndex: -1,
-        //     limit: -1,
-        //     offset: 0
-        // },{
-        //     projections: [],
-        //     querySections: ["Task t", "t.line = line", null, null, null, null, null],
-        //     parentIndex: 0,
-        //     limit: -1,
-        //     offset: 0  
-        // },
-        // {
-        //     projections: ["t1.customer", "t.days","line.name"],
-        //     querySections: ["Task t1", "t1 = t", null, null, null, null, null],
-        //     parentIndex: 1,
-        //     limit: -1,
-        //     offset: 0
-        // });
+        console.log(queries);
         fetch("https://brfenergi.se/task-planner/MakumbaQueryServlet", {
           method: "POST",
           credentials: 'include',
